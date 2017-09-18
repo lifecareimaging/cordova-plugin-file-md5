0e14e65c9a7dcf83d49b9ee750c7c30e21654a0c
root <root@ryan.dev.busivid.com>
2017-09-18 12:52:56 +1000

    Rewrite iOS to avoid memory problems

--- src/ios/md5chksum.m
+++ src/ios/md5chksum.m
@@ -24,27 +24,73 @@
 - (void)file:(CDVInvokedUrlCommand*)command
 {
 	[self.commandDelegate runInBackground:^{
-		CDVPluginResult* pluginResult = nil;
 		NSString *url  = [command.arguments objectAtIndex:0];
 		NSString *path = [self getUrl:url];
-		NSFileHandle *handle = [NSFileHandle fileHandleForReadingAtPath:path];
-		CC_MD5_CTX md5;
-		CC_MD5_Init(&md5);
-		BOOL done = NO;
-		while(!done)
-		{
-			NSData* fileData = [handle readDataOfLength: 4096];
-			CC_MD5_Update(&md5, [fileData bytes], [fileData length]);
-			if( [fileData length] == 0 ) done = YES;
+
+		// Get the file URL
+		CFURLRef fileURL = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, (CFStringRef)path, kCFURLPOSIXPathStyle, (Boolean)false);
+		if (!fileURL)
+			goto done;
+
+		// Create and open the read stream
+		CFReadStreamRef readStream = CFReadStreamCreateWithFile(kCFAllocatorDefault, (CFURLRef)fileURL);
+		if (!readStream)
+			goto done;
+
+		bool didSucceed = (bool)CFReadStreamOpen(readStream);
+		if (!didSucceed)
+			goto done;
+
+		// Initialize the hash object
+		CC_MD5_CTX hashObject;
+		CC_MD5_Init(&hashObject);
+
+		// Feed the data to the hash object
+		bool hasMoreData = true;
+		while (hasMoreData) {
+			uint8_t buffer[4096];
+			CFIndex readBytesCount = CFReadStreamRead(readStream, (UInt8 *)buffer, (CFIndex)sizeof(buffer));
+			if (readBytesCount == -1)
+				break;
+
+			if (readBytesCount == 0) {
+				hasMoreData = false;
+				continue;
+			}
+			CC_MD5_Update(&hashObject, (const void *)buffer, (CC_LONG)readBytesCount);
 		}
+
+		// Check if the read operation succeeded
+		didSucceed = !hasMoreData;
+
+		// Compute the hash digest
 		unsigned char digest[CC_MD5_DIGEST_LENGTH];
-		CC_MD5_Final(digest, &md5);
-		NSString* hex = [NSString stringWithFormat: @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",digest[0], digest[1], digest[2], digest[3], digest[4], digest[5], digest[6], digest[7], digest[8], digest[9], digest[10], digest[11], digest[12], digest[13], digest[14], digest[15]];
-		if (hex != nil && [hex length] > 0) {
-			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:hex];
-		} else {
-			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+		CC_MD5_Final(digest, &hashObject);
+
+		// Abort if the read operation failed
+		if (!didSucceed)
+			goto done;
+
+		// Compute the string result
+		CFStringRef result = NULL;
+		char hash[2 * sizeof(digest) + 1];
+		for (size_t i = 0; i < sizeof(digest); ++i) {
+			snprintf(hash + (2 * i), 3, "%02x", (int)(digest[i]));
 		}
+		result = CFStringCreateWithCString(kCFAllocatorDefault, (const char *)hash, kCFStringEncodingUTF8);
+
+	done:
+		if (readStream) {
+			CFReadStreamClose(readStream);
+			CFRelease(readStream);
+		}
+
+		if (fileURL) {
+			CFRelease(fileURL);
+		}
+
+		CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:(__bridge NSString*)result];
+		CFRelease(result);
 		[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 	}];
 }
