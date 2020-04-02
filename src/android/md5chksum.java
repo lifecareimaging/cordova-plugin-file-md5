0d80d35d9716639ec2971596fa66eaed0170e0a0
mamipu <33715459+mamipu@users.noreply.github.com>
2020-04-02 10:40:01 +0300

    Update md5chksum.java

--- src/android/md5chksum.java
+++ src/android/md5chksum.java
@@ -1,4 +1,4 @@
-package de.fastr.phonegap.plugins;
+package com.manuelramonlopez.cordova.plugins;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -7,8 +7,6 @@ import org.apache.cordova.CordovaResourceApi;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import android.util.Base64;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -64,9 +62,13 @@ public class md5chksum extends CordovaPlugin{
       }
       stream.close();
       byte[] md5chksum = md5.digest();
-      String hex = Base64.encodeToString(md5chksum, Base64.NO_WRAP);
-      callbackContext.success(hex);
-      return true;
+      BigInteger tmp = new BigInteger(1, md5chksum);
+      String hex = tmp.toString(16);
+      hex = ("0000000000000000000000000000000" + hex);
+      hex = hex.substring(hex.length()-32, hex.length());
+			callbackContext.success(hex);
+      
+			return true;
     }catch (FileNotFoundException e){
       callbackContext.error("File not found" + fileUri.toString());
       return false;
